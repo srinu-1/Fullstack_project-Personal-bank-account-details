5704e6f44dad5c39883b5d47771150e4000720af
Yeruva Srinivas Reddy <66551639+srinu-1@users.noreply.github.com>
2022-04-29 18:54:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
 import React from "react";
-import Contact from "./components/Contact";
 import "./app.css";
 
 function App() {
   return (
     <div className="app">
-    <form action="http://127.0.0.1:5000/insert" method="post">
-  <label>
-    firstname:
-    <input type="text" name="first_name" />
-  </label>
-  <label>
-    lastname:
-    <input type="text" name="last_name" />
-  </label>
-  <label>
-    Address:
-    <input type="text" name="address" />
-  </label>
-  <label>
-    Pincode:
-    <input type="text" name="pincode" />
-  </label>
-  <label>
-    Account ID:
-    <input type="text" name="account_id" />
-  </label><label>
-    IFSC:
-    <input type="text" name="ifsc" />
-  </label>
-  <input type="submit" value="Submit" />
+    		<header>PERSONAL & ACCOUNT DETAILS</header>
+
+    <form action="http://127.0.0.1:5000/insert" method="post" className="topBefore">
+
+    <input type="text" name="first_name" placeholder="First Name"/>
+
+    <input type="text" name="last_name" placeholder="Last Name"/>
+
+    <input type="text" name="address" placeholder="Address"/>
+
+    <input type="text" name="pincode" placeholder="pincode"/>
+
+    <input type="text" name="account_id" placeholder="Account Id"/>
+
+    <input type="text" name="ifsc" placeholder="IFSC"/>
+
+    <input type="submit" name="submit"/>
+
+           
+
 </form>
+     
     </div>
   );
 }
